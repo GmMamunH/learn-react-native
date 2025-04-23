22de7aee8f0f4a4fb935d8e159b010f7f246d936
gmmamunh <gmmamun3004@gmail.com>
2025-04-23 22:27:42 +0600

    fetch(server): api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
+      <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png",
+        }}
+        className="w-48 h-48 p-4 mb-4"
+      />
       <Text className="text-3xl text-primary font-bold text-justify px-4 bg-secondary rounded-lg">
         Hello Developer. How are you?{" "}
       </Text>
 
-      <Link href="/about" className="text-2xl p-2 bg-teal-700 rounded-md mt-3 text-white"> About </Link>
+      <Link
+        href="/about"
+        className="text-2xl p-2 bg-teal-700 rounded-md mt-3 text-white"
+      >
+        {" "}
+        About{" "}
+      </Link>
     </View>
   );
 }
--- app/(tabs)/service.tsx
+++ app/(tabs)/service.tsx
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 
-const service = () => {
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const Service = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Post[]>([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-center text-3xl text-red-500">service</Text>
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }) => id.toString()}
+          renderItem={({ item }) => (
+            <View className="border-2 shadow-slate-500 p-3 mb-2 rounded-lg">
+              <Text className="font-bold">{item.title}</Text>
+              <Text className="mb-12">
+                {"\n"}
+                {item.body}
+              </Text>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
-export default service
-
-const styles = StyleSheet.create({})
+export default Service;
