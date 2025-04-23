3530278b68ca2559094c12d54b52a6872dab5cf8
gmmamunh <gmmamun3004@gmail.com>
2025-04-23 14:48:05 +0600

    add(app): global.css, text

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello Developer. How are you? </Text>
     </View>
   );
 }
